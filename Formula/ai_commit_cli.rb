3d0977f149ff98f18419f782213d531d7e6a1f29
hiraishikentaro <silva.ken.97@gmail.com>
2025-04-28 15:34:45 +0900

    Improve installation process using `cargo install`
    The installation process has been updated to use `cargo install` instead of manually building and installing the binary. This simplifies the installation and ensures the binary is installed in the correct location.
    
    Closes #123

--- Formula/ai_commit_cli.rb
+++ Formula/ai_commit_cli.rb
@@ -3,12 +3,12 @@ class AiCommitCli < Formula
   homepage "https://github.com/hiraishikentaro/ai_commit_cli"
   url "https://github.com/hiraishikentaro/ai_commit_cli/archive/refs/tags/v0.0.1.tar.gz"
   version "0.0.1"
+  sha256 "4b1ad2b58490142788826b72d369c93d3f2bd77c6320ba0df1662e8677862c46"
 
   depends_on "rust" => :build
 
   def install
-    system "cargo", "build", "--release"
-    bin.install "target/release/aic"
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
